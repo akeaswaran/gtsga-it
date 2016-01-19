092a719e100e001ae349eb5c98d3c2ac77c42e2a
Akshay Easwaran <akeaswaran@me.com>
2016-01-19 18:51:48 -0500

    card page updates

--- server.js
+++ server.js
@@ -13,9 +13,23 @@ db.on('error', function () {
 
 var app = express();
 
+//App setup
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
 require('./models/card');
 require('./routes')(app);
 
-var port = process.env.PORT || 5000;
-app.listen(port);
-console.log('Listening on port 5000...');
+//post listening
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
